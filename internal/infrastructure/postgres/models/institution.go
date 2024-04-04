367e0794f304f1cb26cabe2316fc21b0c149d5e5
ddaniel27 <danieldorado27@gmail.com>
2024-04-04 15:42:14 -0500

    [UPDATE] institution model fix

--- internal/infrastructure/postgres/models/institution.go
+++ internal/infrastructure/postgres/models/institution.go
@@ -1,12 +1,15 @@
 package models
 
-type CalificationModel struct {
-	ID           uint64  `gorm:"primaryKey"`
-	AverageScore float64 `gorm:"not null"`
-	AverageTime  float64 `gorm:"not null"`
-	UserID       uint64  `gorm:"not null"`
+type InstitutionModel struct {
+	ID       uint64 `gorm:"primary_key"`
+	Name     string `gorm:"unique;not null"`
+	Email    string `gorm:"not null"`
+	Country  string `gorm:"not null"`
+	Province string `gorm:"not null"`
+	City     string `gorm:"not null"`
+	Code     string `gorm:"unique;not null"`
 }
 
-func (CalificationModel) TableName() string {
-	return "pensatta_calification"
+func (InstitutionModel) TableName() string {
+	return "pensatta_institution"
 }
